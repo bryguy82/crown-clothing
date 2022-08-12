55ee75d0e6bbc55d7399dbcf601ca837e5765d64
Anderson-Bryan <43304311+bryguy82@users.noreply.github.com>
2022-08-12 08:55:47 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
+
+const Shop = () => {
+  return (
+    <div>
+      <p>This is the shop page.</p>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
